4e01be8c294da626f6a82b2c45b2389596d0d29d
Breakfulus <breakfulus@proton.me>
2026-06-30 13:35:29 -0400

    Created Game Window And Basic Loop.

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import pygame
 
-print("Hello World!")
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # Events loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen
+    screen.fill("purple")
+
+    # RENDER GAME HERE
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
