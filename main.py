5e80454a2a651cb302e229662540d38dde9a720f
Breakfulus <breakfulus@proton.me>
2026-06-30 17:04:16 -0400

    Added movement and velocity to bullets.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = list(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2))
 bullets = {}
 bullet_count = 0
 timer_list = []
@@ -20,23 +20,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(bullet_count)
+            timer_list.append(1 * 1000)
+            current_pos = (player_pos[0], player_pos[1])
+            bullets[bullet_count] = current_pos
+            bullet_count += 1
+
+
     # Listen for pressed keys; Move if movement keys are pressed
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos[1] -= 300 * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos[1] += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos[0] -= 300 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-    if keys[pygame.K_SPACE]:
-        print(bullet_count)
-        timer_list.append(1 * 1000)
-        current_pos = (pygame.rect.Rect(player_pos.x, player_pos.y, 15, 15).x, pygame.rect.Rect(player_pos.x, player_pos.y, 15, 15).y)
-        bullets[bullet_count] = current_pos
-        bullet_count += 1
+        player_pos[0] += 300 * dt
         
 
     # fill the screen
@@ -46,14 +48,18 @@ while running:
     pygame.draw.circle(screen, "green", player_pos, 30)
 
     bullet_list = bullets.copy().items()
-    for bullet in bullet_list:
-        if timer_list[bullet[0]] == 0:
-            bullets.pop(bullet[0])
+    v_x, v_y = 1, 1
+    for bullet, pos in bullet_list:
+        pos = list(pos)
+        if timer_list[bullet] == 0:
+            bullets.pop(bullet)
         else:
-            timer_list[bullet[0]] -= 1
+            timer_list[bullet] -= 1
         
-            
-        pygame.draw.circle(screen, "red", bullet[1], 15)
+        pos[0] += v_x
+        pos[1] += v_y
+        bullets[bullet] = pos
+        pygame.draw.circle(screen, "red", pos, 15)
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
