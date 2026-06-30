07c2c526a5e512a001533d448ff9f7131e591dc1
Breakfulus <breakfulus@proton.me>
2026-06-30 14:06:31 -0400

    Added simple player movement and delta time.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # Events loop
@@ -15,11 +18,24 @@ while running:
             running = False
 
     # fill the screen
-    screen.fill("purple")
+    screen.fill((26, 27, 33))
 
     # RENDER GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 30)
+
+    # Listen for pressed keys; Move if movement keys are pressed
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
 
     pygame.display.flip()
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
