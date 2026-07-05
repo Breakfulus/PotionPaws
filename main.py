42dc99139fa5f76b3564f0a7550ec86462344dd1
Breakfulus <breakfulus@proton.me>
2026-07-05 12:29:01 -0400

    Added simple game states, enters a lose state when player dies.

--- main.py
+++ main.py
@@ -44,52 +44,68 @@ pygame.time.set_timer(spawn_enemy_event, 1000)
 
 player = Player((screen.get_width() / 2, screen.get_height() / 2), TEMP_PLAYER)
 
+STATE = 0
+
 while running:
-    # Events loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    if STATE == 0:
+        # Events loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
-            direction = (mouse_pos - player.pos).normalize()
-            projectile = Projectile(TEMP, player.pos, direction, "player")
-            bullets.append(projectile)
-        
-        if event.type == SPAWN_ENEMY:
-            new_enemy = Enemy(TEMP_ENEMY, get_next_spawn_point())
-            enemies.append(new_enemy)
-            pygame.time.set_timer(spawn_enemy_event, 1000 * random.randint(1, 5))
-        
-    # Fill the screen
-    screen.fill((26, 27, 33))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+                direction = (mouse_pos - player.pos).normalize()
+                projectile = Projectile(TEMP, player.pos, direction, "player")
+                bullets.append(projectile)
+            
+            if event.type == SPAWN_ENEMY:
+                new_enemy = Enemy(TEMP_ENEMY, get_next_spawn_point())
+                enemies.append(new_enemy)
+                pygame.time.set_timer(spawn_enemy_event, 1000 * random.randint(1, 5))
+            
+        # Fill the screen
+        screen.fill((26, 27, 33))
 
-    for proj in bullets:
+        for proj in bullets:
+            for enemy in enemies:
+                if proj.rect.colliderect(enemy.rect):
+                    enemy.health -= proj.damage
+                    projectile.alive = False
+
+            if proj.alive:
+                proj.update()
+                proj.draw(screen)
+            else:
+                bullets.remove(proj)
+        
+        if player.alive:
+            player.update(dt)
+            player.draw(screen)
+        else:
+            STATE = 1
+
         for enemy in enemies:
-            if proj.rect.colliderect(enemy.rect):
-                enemy.health -= proj.damage
-                projectile.alive = False
+            if enemy.alive and player.alive:
+                if enemy.rect.colliderect(player.rect):
+                    player.health -= enemy.damage
+                enemy.update(player, dt)
+                enemy.draw(screen)
+            else:
+                enemies.remove(enemy)
 
-        if proj.alive:
-            proj.update()
-            proj.draw(screen)
-        else:
-            bullets.remove(proj)
-    
-    if player.alive:
-        player.update(dt)
-        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
-    for enemy in enemies:
-        if enemy.alive and player.alive:
-            if enemy.rect.colliderect(player.rect):
-                player.health -= enemy.damage
-            enemy.update(player, dt)
-            enemy.draw(screen)
-        else:
-            enemies.remove(enemy)
+    if STATE == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # Fill the screen
+        screen.fill((255, 0, 0))
 
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 pygame.quit()
