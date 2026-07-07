a01df658a0b3e3190a57b9def14917655b0ae76a
Breakfulus <breakfulus@proton.me>
2026-07-07 15:35:05 -0400

    Added window caption.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from button import Button
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+pygame.display.set_caption("PotionPaws PROTOTYPE v0.1.0")
 clock = pygame.time.Clock()
 dt = 0
 
